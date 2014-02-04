2b12a810d369926ac0042f4f2763f4b8953f25a6
Max Yankov <golergka@gmail.com>
2014-02-05 00:34:04 +0400

    Stub Percolation class

---
+++ src/Percolation.java
@@ -0,0 +1,23 @@
+
+public class Percolation {
+	
+	public Percolation(int N) {
+		
+	}
+	
+	public void open(int i, int j) {
+		
+	}
+	
+	public boolean isOpen(int i, int j) {
+		return false;
+	}
+	
+	public boolean isFull(int i, int j) {
+		return false;
+	}
+	
+	public boolean percolates() {
+		return false;
+	}
+}
