f183482978e3b0505d2f4ece2d9e1ba613ec72fe
Max Yankov <golergka@gmail.com>
2014-02-05 02:05:02 +0400

    QuickUnion

---
+++ src/IUnionFind.java
@@ -0,0 +1,5 @@
+
+public interface IUnionFind {
+	boolean isConnected(int p, int q);
+	void union(int p, int q);
+}
---
+++ src/QuickUnion.java
@@ -0,0 +1,37 @@
+
+public class QuickUnion implements IUnionFind {
+	
+	protected int[] parents;
+	
+	public QuickUnion(int size)
+	{
+		parents = new int[size];
+		
+		for(int i = 0; i < size; i++)
+			parents[i] = i;
+	}
+	
+	int root(int p)
+	{
+		for(int loopCounter = 0; loopCounter < parents.length; loopCounter++)
+		{
+			if(parents[p] == p)
+				return p;
+			else
+				p = parents[p];
+		}
+		
+		throw new IllegalStateException("Infinite loop detected!");
+	}
+
+	@Override
+	public boolean isConnected(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	@Override
+	public void union(int p, int q) {
+		parents[q] = p;
+	}
+
+}
