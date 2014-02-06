0afa5fe5e6fda5e54c733b49f2a0408446218e8c
Max Yankov <golergka@gmail.com>
2014-02-06 22:43:58 +0400

    Added experiments

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,10 +1,33 @@
 
 public class PercolationStats {
 	
+	int[] m_Experiments;
+	
 	// perform T independent computational experiments on an N-by-N grid
 	public PercolationStats(int N, int T) {
 		if (N <= 0 || T <= 0)
 			throw new IllegalArgumentException();
+		
+		m_Experiments = new int[T];
+		
+		for(int i = 0; i < T; i++)
+			m_Experiments[i] = experiment(N);
+	}
+	
+	int experiment(int N) {
+		int result = 0;
+		Percolation p = new Percolation(N);
+		
+		while(!p.percolates()) {
+			int i = StdRandom.uniform(N)+1;
+			int j = StdRandom.uniform(N)+1;
+			if (!p.isOpen(i, j)) {
+				result++;
+				p.open(i, j);
+			}
+		}
+		
+		return result;
 	}
 	
 	// sample mean of percolation threshold
