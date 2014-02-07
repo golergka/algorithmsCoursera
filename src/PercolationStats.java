443e29f7355cd21fdc81895b9fff684420b1871d
Max Yankov <golergka@gmail.com>
2014-02-07 23:54:20 +0400

    PercolationStats complete

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,17 +1,25 @@
 
 public class PercolationStats {
 	
-	int[] m_Experiments;
+	final int[] m_Experiments;
+	final double[] m_Fractions;
+	final int m_Size;
 	
 	// perform T independent computational experiments on an N-by-N grid
 	public PercolationStats(int N, int T) {
 		if (N <= 0 || T <= 0)
 			throw new IllegalArgumentException();
 		
+		m_Size = N;
+		
 		m_Experiments = new int[T];
+		m_Fractions = new double[T];
 		
 		for(int i = 0; i < T; i++)
+		{
 			m_Experiments[i] = experiment(N);
+			m_Fractions[i] = m_Experiments[i] / (double) (m_Size * m_Size);
+		}
 	}
 	
 	int experiment(int N) {
@@ -19,8 +27,8 @@ public class PercolationStats {
 		Percolation p = new Percolation(N);
 		
 		while(!p.percolates()) {
-			int i = StdRandom.uniform(N)+1;
-			int j = StdRandom.uniform(N)+1;
+			int i = StdRandom.uniform(1, N+1);
+			int j = StdRandom.uniform(1, N+1);
 			if (!p.isOpen(i, j)) {
 				result++;
 				p.open(i, j);
@@ -32,25 +40,34 @@ public class PercolationStats {
 	
 	// sample mean of percolation threshold
 	public double mean() {
-		return 0;
+		return StdStats.mean(m_Fractions);
 	}
 	
 	// sample standard deviation of percolation threshold
 	public double stddev() {
-		return 0;
+		return StdStats.stddev(m_Fractions);
 	}
 	
 	// returns lower bound of the 95% confidence interval
 	public double confidenceLo() {
-		return 0;
+		return mean() - 1.96 * stddev() / Math.sqrt(m_Experiments.length);
 	}
 	
 	// returns upper bound of the 95% confidence interval
 	public double confidenceHi() {
-		return 0;
+		return mean() + 1.96 * stddev() / Math.sqrt(m_Experiments.length);
 	}
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
+		if (args.length < 2)
+			StdOut.println("Should have 2 arguments!");
 		
+		int size = Integer.parseInt(args[0]);
+		int experiments = Integer.parseInt(args[1]);
+		
+		PercolationStats p = new PercolationStats(size, experiments);
+		StdOut.println("mean                    = " + p.mean());
+		StdOut.println("stddev                  = " + p.stddev());
+		StdOut.println("95% confidence interval = " + p.confidenceLo() + ", " + p.confidenceHi());
 	}
 }
--- src/TestPercolationStats.java
+++ src/TestPercolationStats.java
@@ -22,11 +22,21 @@ public class TestPercolationStats {
 		}
 	}
 	
+	public void testMain(int N, int T) {
+		StdOut.println("testMain " + N + " " + T);
+		PercolationStats.main(new String[] {Integer.toString(N), Integer.toString(T)});
+	}
+	
 	public static void main(String[] args) {
 		StdOut.println("Starting tests... ");
 		
 		TestPercolationStats tester = new TestPercolationStats();
 		tester.testConstructorArgumentBounds();
+		
+		tester.testMain(200, 100);
+		tester.testMain(200, 100);
+		tester.testMain(2, 10000);
+		tester.testMain(2, 10000);
 		
 		StdOut.println("Tests complete.");
 	}
