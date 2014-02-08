2c2863602052c8f2f171a41b8a11bf2903185e26
Max Yankov <golergka@gmail.com>
2014-02-08 04:19:38 +0400

    checkstyle fixes

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,98 +1,103 @@
 
 public class Percolation {
-	
-	private boolean [][] m_Open;
-	
-	private final int m_Size;
-	
-	private WeightedQuickUnionUF m_UnionFind;
-	
-	private boolean[] m_Full;
-	private boolean[] m_Grounded;
-	
-	private boolean m_Percolates = false;
-	
-	public Percolation(int N) {
-		m_Size = N;
-		m_Open = new boolean[N+1][N+1];
-		m_Full = new boolean[N*N];
-		m_Grounded = new boolean[N*N];
-		m_UnionFind = new WeightedQuickUnionUF(N*N);
-	}
-	
-	private int coordinatesToInt(int i, int j) {
-		return (i-1)*m_Size + (j-1);
-	}
-	
-	private void unionIfOpen(int i1, int j1, int i2, int j2) {
-		
-		if (m_Open[i1][j1] && m_Open[i2][j2])
-		{
-			boolean filled = isFull(i1,j2) || isFull(i2, j2);
-			boolean grounded = isGrounded(i1,j2) || isGrounded(i2, j2);
-			
-			m_UnionFind.union(coordinatesToInt(i1,j1), coordinatesToInt(i2,j2));
-			
-			if (filled) fill(i1,j1);
-			if (grounded) ground(i2, j2);
-		}
-	}
-	
-	private boolean validCoordinate(int i) {
-		return !(i < 1 || i > m_Size);
-	}
-	
-	private void checkCoordinate(int i) {
-		if (!validCoordinate(i))
-			throw new java.lang.IndexOutOfBoundsException("Coordinate outside of range: " + i + " [1;" + m_Size + "]");
-	}
-	
-	public void open(int i, int j) {
-		checkCoordinate(i);
-		checkCoordinate(j);
-		
-		m_Open[i][j] = true;
-		
-		// Connecting to the top an bottom
-		if (i == 1)
-			fill(i,j);
-		if (i == m_Size)
-			ground(i,j);
-		
-		if (i > 1)
-			unionIfOpen(i, j, i-1, j);
-		if (i < m_Size)
-			unionIfOpen(i, j, i+1, j);
-		if (j > 1)
-			unionIfOpen(i, j, i, j-1);
-		if (j < m_Size)
-			unionIfOpen(i, j, i, j+1);
-		
-		if (isFull(i,j) && isGrounded(i,j))
-			m_Percolates = true;
-	}
-	
-	public boolean isOpen(int i, int j) {
-		return m_Open[i][j];
-	}
-	
-	public boolean isFull(int i, int j) {
-		return m_Full[m_UnionFind.find(coordinatesToInt(i,j))];
-	}
-	
-	private void fill(int i, int j) {
-		m_Full[m_UnionFind.find(coordinatesToInt(i,j))] = true;
-	}
-	
-	private boolean isGrounded(int i, int j) {
-		return m_Grounded[m_UnionFind.find(coordinatesToInt(i,j))];
-	}
-	
-	private void ground(int i, int j) {
-		m_Grounded[m_UnionFind.find(coordinatesToInt(i,j))] = true;
-	}
-	
-	public boolean percolates() {
-		return m_Percolates;
-	}
+
+    private boolean [][] open;
+
+    private final int size;
+
+    private WeightedQuickUnionUF unionFind;
+
+    private boolean[] full;
+    private boolean[] grounded;
+
+    private boolean percolates = false;
+
+    public Percolation(int N) {
+        size = N;
+        open = new boolean[N+1][N+1];
+        full = new boolean[N*N];
+        grounded = new boolean[N*N];
+        unionFind = new WeightedQuickUnionUF(N*N);
+    }
+
+    private int coordinatesToInt(int i, int j) {
+        return (i-1)*size + (j-1);
+    }
+
+    private void unionIfOpen(int i1, int j1, int i2, int j2) {
+
+        if (open[i1][j1] && open[i2][j2])
+        {
+            boolean thisFilled = isFull(i1, j2) || isFull(i2, j2);
+            boolean thisGrounded = isGrounded(i1, j2) || isGrounded(i2, j2);
+
+            unionFind.union(
+                    coordinatesToInt(i1, j1),
+                    coordinatesToInt(i2, j2)
+                    );
+
+            if (thisFilled) fill(i1, j1);
+            if (thisGrounded) ground(i2, j2);
+        }
+    }
+
+    private boolean validCoordinate(int i) {
+        return !(i < 1 || i > size);
+    }
+
+    private void checkCoordinate(int i) {
+        if (!validCoordinate(i))
+            throw new java.lang.IndexOutOfBoundsException(
+                    "Coordinate outside of range: " + i + " [1;" + size + "]"
+                    );
+    }
+
+    public void open(int i, int j) {
+        checkCoordinate(i);
+        checkCoordinate(j);
+
+        open[i][j] = true;
+
+        // Connecting to the top an bottom
+        if (i == 1)
+            fill(i, j);
+        if (i == size)
+            ground(i, j);
+
+        if (i > 1)
+            unionIfOpen(i, j, i-1, j);
+        if (i < size)
+            unionIfOpen(i, j, i+1, j);
+        if (j > 1)
+            unionIfOpen(i, j, i, j-1);
+        if (j < size)
+            unionIfOpen(i, j, i, j+1);
+
+        if (isFull(i, j) && isGrounded(i, j))
+            percolates = true;
+    }
+
+    public boolean isOpen(int i, int j) {
+        return open[i][j];
+    }
+
+    public boolean isFull(int i, int j) {
+        return full[unionFind.find(coordinatesToInt(i, j))];
+    }
+
+    private void fill(int i, int j) {
+        full[unionFind.find(coordinatesToInt(i, j))] = true;
+    }
+
+    private boolean isGrounded(int i, int j) {
+        return grounded[unionFind.find(coordinatesToInt(i, j))];
+    }
+
+    private void ground(int i, int j) {
+        grounded[unionFind.find(coordinatesToInt(i, j))] = true;
+    }
+
+    public boolean percolates() {
+        return percolates;
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,73 +1,72 @@
-
 public class PercolationStats {
-	
-	private final int[] m_Experiments;
-	private final double[] m_Fractions;
-	private final int m_Size;
-	
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		if (N <= 0 || T <= 0)
-			throw new IllegalArgumentException();
-		
-		m_Size = N;
-		
-		m_Experiments = new int[T];
-		m_Fractions = new double[T];
-		
-		for(int i = 0; i < T; i++)
-		{
-			m_Experiments[i] = experiment(N);
-			m_Fractions[i] = m_Experiments[i] / (double) (m_Size * m_Size);
-		}
-	}
-	
-	private int experiment(int N) {
-		int result = 0;
-		Percolation p = new Percolation(N);
-		
-		while(!p.percolates()) {
-			int i = StdRandom.uniform(1, N+1);
-			int j = StdRandom.uniform(1, N+1);
-			if (!p.isOpen(i, j)) {
-				result++;
-				p.open(i, j);
-			}
-		}
-		
-		return result;
-	}
-	
-	// sample mean of percolation threshold
-	public double mean() {
-		return StdStats.mean(m_Fractions);
-	}
-	
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		return StdStats.stddev(m_Fractions);
-	}
-	
-	// returns lower bound of the 95% confidence interval
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(m_Experiments.length);
-	}
-	
-	// returns upper bound of the 95% confidence interval
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(m_Experiments.length);
-	}
-	
-	public static void main(String[] args) {
-		if (args.length < 2)
-			StdOut.println("Should have 2 arguments!");
-		
-		int size = Integer.parseInt(args[0]);
-		int experiments = Integer.parseInt(args[1]);
-		
-		PercolationStats p = new PercolationStats(size, experiments);
-		StdOut.println("mean                    = " + p.mean());
-		StdOut.println("stddev                  = " + p.stddev());
-		StdOut.println("95% confidence interval = " + p.confidenceLo() + ", " + p.confidenceHi());
-	}
+
+    private final int[] experiments;
+    private final double[] fractions;
+    private final int size;
+
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0)
+            throw new IllegalArgumentException();
+
+        size = N;
+
+        experiments = new int[T];
+        fractions = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            experiments[i] = experiment(N);
+            fractions[i] = experiments[i] / (double) (size * size);
+        }
+    }
+
+    private int experiment(int N) {
+        int result = 0;
+        Percolation p = new Percolation(N);
+
+        while (!p.percolates()) {
+            int i = StdRandom.uniform(1, N + 1);
+            int j = StdRandom.uniform(1, N + 1);
+            if (!p.isOpen(i, j)) {
+                result++;
+                p.open(i, j);
+            }
+        }
+
+        return result;
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(experiments.length);
+    }
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(experiments.length);
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2)
+            StdOut.println("Should have 2 arguments!");
+
+        int size = Integer.parseInt(args[0]);
+        int experiments = Integer.parseInt(args[1]);
+
+        PercolationStats p = new PercolationStats(size, experiments);
+        StdOut.println("mean                    = " + p.mean());
+        StdOut.println("stddev                  = " + p.stddev());
+        StdOut.println("95% confidence interval = " + p.confidenceLo() + ", "
+                + p.confidenceHi());
+    }
 }
