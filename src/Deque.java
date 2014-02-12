cdf3f965cd0a2fff8d21a13b816bc7ba8a952e1e
Max Yankov <golergka@gmail.com>
2014-02-13 02:20:19 +0400

    Unimplemented Deque methods

--- src/Deque.java
+++ src/Deque.java
@@ -9,10 +9,34 @@ public class Deque<Item> implements Iterable<Item> {
     public boolean isEmpty() {
         throw new UnsupportedOperationException();
     }
+    
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addFirst(Item item) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public void addLast(Item item) {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Item removeFirst() {
+        throw new UnsupportedOperationException();
+    }
+    
+    public Item removeLast() {
+        throw new UnsupportedOperationException();
+    }
 
     @Override
     public Iterator<Item> iterator() {
         throw new UnsupportedOperationException();
+    }
+    
+    public static void main(String[] args) {
+        
     }
 
 }
