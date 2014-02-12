8f6c2123d7f1df0b618798a4f1e58dcb45c5d6dc
Max Yankov <golergka@gmail.com>
2014-02-13 01:52:01 +0400

    Deque stub

---
+++ src/Deque.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+
+public class Deque<Item> implements Iterable<Item> {
+    
+    public Deque() {
+    }
+    
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+}
