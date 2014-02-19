7ca7376c6f7b28877c45c4b9727b7ef5448e5326
Max Yankov <golergka@gmail.com>
2014-02-19 23:00:16 +0400

    RandomizedQueue stub

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    public RandomizedQueue() {
+        
+    }
+    
+    public boolean isEmpty() {
+        return true;
+    }
+    
+    public int size() {
+        return 0;
+    }
+    
+    public void enqueue(Item item) {
+        
+    }
+    
+    public Item dequeue() {
+        return null;
+    }
+    
+    public Item sample() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
