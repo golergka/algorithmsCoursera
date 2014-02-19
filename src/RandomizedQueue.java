2f7b260e0b74063867e63efec3d32de75961efd8
Max Yankov <golergka@gmail.com>
2014-02-19 23:50:26 +0400

    Basic RandomizedQueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,30 +1,63 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     
+    private Item[] contents;
+    private int N = 0;
+    
+    @SuppressWarnings("unchecked")
     public RandomizedQueue() {
-        
+        contents = (Item[]) new Object[1];
     }
     
     public boolean isEmpty() {
-        return true;
+        return N == 0;
     }
     
     public int size() {
-        return 0;
+        return N;
     }
     
     public void enqueue(Item item) {
-        
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        contents[N++] = item;
+        if (N == contents.length) {
+            @SuppressWarnings("unchecked")
+            Item[] newContents = (Item[]) new Object[N*2];
+            for(int i = 0; i < contents.length; i++) {
+                newContents[i] = contents[i];
+            }
+            contents = newContents;
+        }
     }
     
     public Item dequeue() {
-        return null;
+        Item result = sample();
+        N--;
+        if (N < contents.length/4) {
+            @SuppressWarnings("unchecked")
+            Item[] newContents = (Item[]) new Object[contents.length/2];
+            for(int i = 0; i < contents.length/2; i++) {
+                newContents[i] = contents[i];
+            }
+            contents = newContents;
+        }
+        return result;
     }
     
     public Item sample() {
-        return null;
+        if (N == 0) {
+            throw new NoSuchElementException();
+        }
+        
+        int randomIndex = StdRandom.uniform(N);
+        Item result = contents[randomIndex];
+        contents[randomIndex] = contents[N-1];
+        return result;
     }
 
     @Override
